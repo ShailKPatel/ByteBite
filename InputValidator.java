b93ae15b84f73d1b5f50c2461556933e19f0b2cb
Shail K Patel <114606340+ShailKPatel@users.noreply.github.com>
2024-03-01 01:39:05 +0530

    Create InputValidator.java

---
+++ InputValidator.java
@@ -0,0 +1,62 @@
+package ByteBites;
+
+import java.util.Scanner;
+
+public class InputValidator {
+    // This program will make sure that you don't accidentally crash the program
+    // when you enter something other than an int
+
+    private Scanner scanner;
+
+    InputValidator() {
+        scanner = new Scanner(System.in);
+    }
+
+    int getIntInput(String prompt) {
+        scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.print(prompt);
+                String userInput = scanner.nextLine();
+                int input = Integer.parseInt(userInput);
+                return input;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Input. Please Enter a Valid Integer.");
+            }
+        }
+    }
+
+    int getPositiveIntInput(String prompt) {
+        while (true) {
+            try {
+                System.out.print(prompt);
+                String userInput = scanner.nextLine();
+                int input = Integer.parseInt(userInput);
+
+                // Check if input is positive
+                if (input > 0) {
+                    return input;
+                } else {
+                    System.out.println("Invalid Input. Please Enter a Positive Integer.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Input. Please Enter a Valid Integer.");
+            }
+        }
+    }
+
+    String getStringInput(String prompt) {
+        while (true) {
+            System.out.print(prompt);
+            String userInput = scanner.nextLine();
+
+            if (userInput.length() <= 32) {
+                return userInput;
+            } else {
+                System.out.println("Invalid Input. Please Enter a String of 32 Characters or Shorter.");
+            }
+        }
+    }
+
+}
