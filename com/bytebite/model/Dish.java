12b7fba898e0520751783bc95916e8178b24b754
Shail K Patel <114606340+ShailKPatel@users.noreply.github.com>
2024-06-08 21:02:18 +0530

    Update Dish.java

--- com/bytebite/model/Dish.java
+++ com/bytebite/model/Dish.java
@@ -1,14 +1,12 @@
-// LOC:
-// Dish() : 
+//LOC:
 // void setDishCourse() : 
 // void setDishCuisine() : 
 // void setDishDiet() : 
 // void setDishPrice() : 
 // void setDishPreparationTime() : 
 
-
-
 import java.util.ArrayList;
+import com.bytebite.util.InputValidator;
 
 public class Dish {
 
@@ -43,20 +41,41 @@ public class Dish {
     private void setDishCourse() {
         System.out.println();
         int choice = iv.getIntInput(
-                "1. Appetizer\n2. Soup\n3. Salad\n4. Entree\n5. Main Course\n6. Side Dish\n7. Dessert\n8. Beverage\n9. Palate Cleanser\n10. Other\n" +
-                "Choose Course: ");
+                "1. Appetizer\n2. Soup\n3. Salad\n4. Entree\n5. Main Course\n6. Side Dish\n7. Dessert\n8. Beverage\n9. Palate Cleanser\n10. Other\n"
+                        +
+                        "Choose Course: ");
 
         switch (choice) {
-            case 1: this.dishCourse = "Appetizer"; break;
-            case 2: this.dishCourse = "Soup"; break;
-            case 3: this.dishCourse = "Salad"; break;
-            case 4: this.dishCourse = "Entree"; break;
-            case 5: this.dishCourse = "Main Course"; break;
-            case 6: this.dishCourse = "Side Dish"; break;
-            case 7: this.dishCourse = "Dessert"; break;
-            case 8: this.dishCourse = "Beverage"; break;
-            case 9: this.dishCourse = "Palate Cleanser"; break;
-            case 10: this.dishCourse = "Other"; break;
+            case 1:
+                this.dishCourse = "Appetizer";
+                break;
+            case 2:
+                this.dishCourse = "Soup";
+                break;
+            case 3:
+                this.dishCourse = "Salad";
+                break;
+            case 4:
+                this.dishCourse = "Entree";
+                break;
+            case 5:
+                this.dishCourse = "Main Course";
+                break;
+            case 6:
+                this.dishCourse = "Side Dish";
+                break;
+            case 7:
+                this.dishCourse = "Dessert";
+                break;
+            case 8:
+                this.dishCourse = "Beverage";
+                break;
+            case 9:
+                this.dishCourse = "Palate Cleanser";
+                break;
+            case 10:
+                this.dishCourse = "Other";
+                break;
             default:
                 System.out.println("Invalid Choice\nSetting Course To Other");
                 this.dishCourse = "Other";
@@ -77,16 +96,35 @@ public class Dish {
                     "1. Indian\n2. South Indian\n3. Gujarati\n4. Punjabi\n5. Chinese\n6. Japanese\n7. Italian\n8. Mexican\n9. Continental\n10. Custom\nChoose Cuisine Tags: ");
 
             switch (choice) {
-                case 0: break;
-                case 1: this.dishCuisineTags.add("Indian"); break;
-                case 2: this.dishCuisineTags.add("South Indian"); break;
-                case 3: this.dishCuisineTags.add("Gujarati"); break;
-                case 4: this.dishCuisineTags.add("Punjabi"); break;
-                case 5: this.dishCuisineTags.add("Chinese"); break;
-                case 6: this.dishCuisineTags.add("Japanese"); break;
-                case 7: this.dishCuisineTags.add("Italian"); break;
-                case 8: this.dishCuisineTags.add("Mexican"); break;
-                case 9: this.dishCuisineTags.add("Continental"); break;
+                case 0:
+                    break;
+                case 1:
+                    this.dishCuisineTags.add("Indian");
+                    break;
+                case 2:
+                    this.dishCuisineTags.add("South Indian");
+                    break;
+                case 3:
+                    this.dishCuisineTags.add("Gujarati");
+                    break;
+                case 4:
+                    this.dishCuisineTags.add("Punjabi");
+                    break;
+                case 5:
+                    this.dishCuisineTags.add("Chinese");
+                    break;
+                case 6:
+                    this.dishCuisineTags.add("Japanese");
+                    break;
+                case 7:
+                    this.dishCuisineTags.add("Italian");
+                    break;
+                case 8:
+                    this.dishCuisineTags.add("Mexican");
+                    break;
+                case 9:
+                    this.dishCuisineTags.add("Continental");
+                    break;
                 case 10:
                     String customCuisine = iv.getStringInput32Char("Enter Custom Cuisine: ");
                     this.dishCuisineTags.add(customCuisine);
@@ -111,16 +149,44 @@ public class Dish {
             choice = iv.getIntInput(
                     "1. Vegetarian\n2. Vegan\n3. Keto\n4. Gluten-Free\n5. Low-Carb\n6. Paleo\n7. Dairy-Free\n8. Nut-Free\n9. Sugar-Free\n10. Custom\nChoose Diet Tags: ");
             switch (choice) {
-                case 0: break;
-                case 1: this.dishDietTags.add("Vegetarian"); counter++; break;
-                case 2: this.dishDietTags.add("Vegan"); counter++; break;
-                case 3: this.dishDietTags.add("Keto"); counter++; break;
-                case 4: this.dishDietTags.add("Gluten-Free"); counter++; break;
-                case 5: this.dishDietTags.add("Low-Carb"); counter++; break;
-                case 6: this.dishDietTags.add("Paleo"); counter++; break;
-                case 7: this.dishDietTags.add("Dairy-Free"); counter++; break;
-                case 8: this.dishDietTags.add("Nut-Free"); counter++; break;
-                case 9: this.dishDietTags.add("Sugar-Free"); counter++; break;
+                case 0:
+                    break;
+                case 1:
+                    this.dishDietTags.add("Vegetarian");
+                    counter++;
+                    break;
+                case 2:
+                    this.dishDietTags.add("Vegan");
+                    counter++;
+                    break;
+                case 3:
+                    this.dishDietTags.add("Keto");
+                    counter++;
+                    break;
+                case 4:
+                    this.dishDietTags.add("Gluten-Free");
+                    counter++;
+                    break;
+                case 5:
+                    this.dishDietTags.add("Low-Carb");
+                    counter++;
+                    break;
+                case 6:
+                    this.dishDietTags.add("Paleo");
+                    counter++;
+                    break;
+                case 7:
+                    this.dishDietTags.add("Dairy-Free");
+                    counter++;
+                    break;
+                case 8:
+                    this.dishDietTags.add("Nut-Free");
+                    counter++;
+                    break;
+                case 9:
+                    this.dishDietTags.add("Sugar-Free");
+                    counter++;
+                    break;
                 case 10:
                     String customTag = iv.getStringInput32Char("Enter Custom Diet Tag: ");
                     this.dishDietTags.add(customTag);
